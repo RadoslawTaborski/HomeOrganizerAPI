c06ba22bcc73c921a4a38290a255f8734efd38fa
Radoslaw Taborski <radoslawtaborski@gmail.com>
2021-09-07 22:03:35 +0200

    fixed script002

--- HomeOrganizerAPI/DatabaseScripts/script01.sql
+++ HomeOrganizerAPI/DatabaseScripts/script01.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `user` ADD `external_uuid` VARCHAR(255) NOT NULL AFTER `username`;
 CREATE INDEX `external_uuid_idx` ON `user` (`external_uuid`);
-UPDATE `home_organizer`.`user` U JOIN `AuthServer`.`AspNetUser` AU ON AU.Email = U.email SET U.`external_uuid` = AU.Id;
+UPDATE `home_organizer`.`user` U JOIN `AuthServer`.`AspNetUsers` AU ON AU.Email = U.email SET U.`external_uuid` = AU.Id;
 ALTER TABLE `user` DROP `email`, DROP `password`;
